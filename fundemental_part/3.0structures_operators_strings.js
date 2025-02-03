2d6d965abc46422271bdcf2cafa5967d35656055
Minijie3 <1208352126@qq.com>
2025-02-03 16:52:59 +0800

    2nd commit

--- fundemental_part/3.0structures_operators_strings.js
+++ fundemental_part/3.0structures_operators_strings.js
@@ -95,3 +95,141 @@ this equals to 'console.log(undefined.open)', TypeError will be thrown.
 */
 console.log(restaurant.italianRequest?.('tomatoes, peanut') ?? 'There is no such functions.')
 // There is no such functions.
+
+const [properties, values] = [Object.keys(restaurant.openingHours), Object.values(restaurant.openingHours)];
+console.log(properties); // [ 'monToFri', 'sat', 'sun' ]
+console.log(values);
+/*
+[
+  { open: 10, close: 22 },
+  { open: 11, close: 23 },
+  { open: 12, close: 24 }
+]
+*/
+// Similar to dictionary in Python.
+console.log(Object.entries(restaurant.openingHours));
+/*
+[
+  [ 'monToFri', { open: 10, close: 22 } ],
+  [ 'sat', { open: 11, close: 23 } ],
+  [ 'sun', { open: 12, close: 24 } ]
+]
+*/
+/*
+NOTICE!
+for ... of can only be used in iterabel things. For example:
+game.score = [1, 2, 3, 4, 5];
+if write:
+for (const [i, item] of [game.score.length, game.score])
+it will throw an error, cause this code means that game.score.length is iterabel.
+So if we want to get the index of game.score, it should be:
+for (const [i, item] of game.score.entries())
+cause for objects: entries() method will return [[key1, value1], [key2, value2]...]
+but for arrays: entries() method will return [[index1, value1], [index2, value2]...]
+then JavaScript will take [i, item] = [index, value].
+but notice that for objects, entries method should be written like: Object.entries(objectName)
+*/
+
+for (const [day, { open: openTime, close: closeTime }] of Object.entries(restaurant.openingHours)) {
+    console.log(`On ${day}, we open at ${openTime}:00 and close at ${closeTime}:00.`);
+};
+
+const testSet = new Set(['a', 'b', 'c', 'a', 'b', 'c']);
+console.log(testSet);// Set(3) { 'a', 'b', 'c' }
+testSet.add('d');
+testSet.delete('b');
+console.log(testSet, testSet.has('a'), testSet.size);// Set(3) { 'a', 'c', 'd' } true 3
+/*
+Just like sets in Python or math, The values in a set are distinct, and the order does not matter,
+which means that it is meaningless to extract values from it; 
+therefore, sets have no way to obtain index and value.
+BUT looping is valid just like other iterables.
+And NOTICE that new sets(ITERABELS).
+*/
+for (const item of testSet) console.log(item);// that's OK.
+const staff = ['manager', 'waiter', 'chef'];
+console.log([...new Set(staff)]);//set → array
+console.log(new Set('jonasschmedtmann'));// { 'j', 'o', 'n', 'a', 's', 'c', 'h', 'm', 'e', 'd', 't' }
+console.log(new Set('sanqiantiekuai is a nice woman').size);// count the unique letters in a string.
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+    .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, 'We are closed :(');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
+/* 
+What should be NOTICE is that if write "rest.set([1, 2], 'Test)'", 
+it won't work when we use "rest.get([1, 2])".
+Because the array is a reference type, and the array is not the same as the array in the map.
+So arrays must be stored in a certain variable.
+*/
+rest.clear()
+rest.set([
+    ['question', 'who\'s the most beautiful girl in the world?'],
+    ['1', 'answer1'],
+    ['2', 'answer2'],
+    ['3', 'sanqiantiekuai'],
+    ['correct', 3],
+    [true, 'correct!'],
+    [false, 'u a wrong.']
+]);
+console.log(rest);
+const restAnother = new Map([
+    ['question', 'who\'s the most beautiful girl in the world?'],
+    [1, 'answer1'],
+    [2, 'answer2'],
+    [3, 'sanqiantiekuai'],
+    ['correct', 3],
+    [true, 'correct!'],
+    [false, 'u a wrong.']
+]);
+console.log(restAnother);
+/*
+The result of the two:
+Map(1) {
+  [
+    [ 'question', "who's the most beautiful girl in the world?" ],
+    [ '1', 'answer1' ],
+    [ '2', 'answer2' ],
+    [ '3', 'sanqiantiekuai' ],
+    [ 'correct', 3 ],
+    [ true, 'correct!' ],
+    [ false, 'u a wrong.' ]
+  ] => undefined
+}
+Map(7) {
+  'question' => "who's the most beautiful girl in the world?",
+  '1' => 'answer1',
+  '2' => 'answer2',
+  '3' => 'sanqiantiekuai',
+  'correct' => 3,
+  true => 'correct!',
+  false => 'u a wrong.'
+}
+According to the result, object can be converted to Map.
+*/
+const restaurantMap = new Map(Object.entries(restaurant));
+console.log(restaurantMap);
+// Map is also iterable
+console.log(restAnother.get('question'));
+for (const [index, answer] of restAnother.entries()) {
+    if (typeof index === 'number') console.log(`Answer ${index}: ${answer}`);
+};
+console.log(...restAnother, ...rest);// latter: undefined
--- fundemental_part/3.1tiny_tries.js
+++ fundemental_part/3.1tiny_tries.js
@@ -97,3 +97,47 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 // 7
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
+
+/*
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+*/
+
+// 1
+for (const [goalNumber, playerName] of game.scored.entries()) {
+    console.log(`Goal ${goalNumber + 1}: ${playerName}`);
+}
+
+// 2
+let sums = 0;
+for (const oddItem of Object.values(game.odds)) {
+    sums += oddItem;
+}
+const averageOdds = sums / Object.values(game.odds).length;
+console.log(averageOdds);
+
+// 3
+for (const [team, oddItem] of Object.entries(game.odds)) {
+    const teamStr = (team === 'x' && 'draw') || team;
+    console.log(`Odd of victory ${teamStr}: ${oddItem}`);
+}
+
+const scorers = {};
+for (const player of game.scored) {
+    scorers[player] ? scorers[player]++ : scorers[player] = 1;
+}
+console.log(scorers);
